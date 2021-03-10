e465d4a1e527c0151c79ca5ec42c1c09b79b4ed6
Namrathagaekwade <namrathagaekwade@gmail.com>
2021-03-10 14:09:56 +0100

    FIRST COMMIT

---
+++ Test_example1.py
@@ -0,0 +1,47 @@
+__author__ = 'JYOTHSNA'
+import unittest
+import Simple_Calculator
+
+#A series of tests designed to see if the functions fail
+class ArithTest (unittest.TestCase):
+
+    def runTest (self):
+
+
+        self.failUnless(Simple_Calculator.add(1, 2) == 3, msg='1+2 = 3 failed')
+
+        self.failIf(Simple_Calculator.divide(10, 2) == 3, msg='10/2 = 3 fail test failed')
+
+        self.failUnlessEqual(Simple_Calculator.multiply(2, 2), 4, msg='2 * 2 failed')
+
+        self.assertTrue(Simple_Calculator.subtract(10, 3) == 7, msg='10 - 3 failed')
+
+        self.assertTrue(Simple_Calculator.multiply(5, 5) == 25, msg='5 * 5 failed')
+
+        self.assertEqual(Simple_Calculator.add(10, 15), 25, msg='10 + 15 = 25 failed')
+
+        self.assertEqual(Simple_Calculator.divide(50, 10), 5, msg='50 / 10 failed')
+
+        self.assertFalse(Simple_Calculator.subtract(20, 5) == 14, msg='20 - 5 = 14 fail test failed')
+
+
+
+def suite():
+
+    suite = unittest.TestSuite()
+
+    suite.addTest (ArithTest())
+
+    return suite
+
+
+
+
+
+if __name__ == '__main__':
+
+    runner = unittest.TextTestRunner()
+
+    test_suite = suite()
+
+    runner.run (test_suite)
